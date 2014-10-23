94669241a38701590dc70520d97d6c524d5ac81e
Abdullah <abd00s@users.noreply.github.com>
2014-10-23 14:42:19 -0400

    complete exercise 1

--- people.rb
+++ people.rb
@@ -1,11 +1,38 @@
-class Student
-	def learn
-		"I get it!"
+class Person
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+	def greet
+		puts "My name is #{@name}"
 	end
 end
 
-class Instructor
-	def teach
-		"Everything in Ruby is an Object"
+class Student < Person
+	def learn
+		puts "I get it!"
 	end
 end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
+end
+
+chris = Instructor.new("Chris")
+chris.greet
+
+christina = Student.new("Christina")
+christina.greet
+
+chris.teach
+
+christina.learn
+					#|==============================================================|
+chris.learn			#| Methods are being called on classes where theyre not defined |
+					#| Error message:  												|
+christina.teach 	#| NoMethodError												|
+					#|==============================================================|
